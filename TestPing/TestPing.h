33c2b69d85324214168a6071f88fe560940b61b2
Igor Kovaltsov <kovaltsov@hotmail.com>
2023-05-07 14:50:33 +0300

    first init

---
+++ TestPing/TestPing.h
@@ -0,0 +1,32 @@
+﻿
+// TestPing.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CTestPingApp:
+// Сведения о реализации этого класса: TestPing.cpp
+//
+
+class CTestPingApp : public CWinApp
+{
+public:
+	CTestPingApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestPingApp theApp;
---
+++ TestPing/TestPingDlg.h
@@ -0,0 +1,34 @@
+﻿
+// TestPingDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CTestPingDlg
+class CTestPingDlg : public CDialogEx
+{
+// Создание
+public:
+	CTestPingDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TESTPING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
